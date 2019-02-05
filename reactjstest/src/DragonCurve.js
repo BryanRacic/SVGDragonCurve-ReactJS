2826497e1ca1d3385db3e120da4bbfd208801512
BryanLikesToProgram <bryan.racic@angrysquirrels.com>
2019-02-04 22:48:14 -0500

    Animation & New Architecture

--- reactjstest/src/DragonCurve.js
+++ reactjstest/src/DragonCurve.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
 
 class DragonCurve extends Component {
-    render( props ) {
-        console.log( "DragonCurve.js" );
-        console.log( "frame: " + this.props.frame );
-        return (
-            <CurveAnimate width="500" height="500" counter="0" />
-        )
-    }
-}
-//var pathString = "M250 250 V350 H 350";
-var lineSegments = [ 0, 0, 0, 1 ];
-var lineCount = ( lineSegments.length / 2 - 1 );
-var lineCoord = "0,0 0,1";
-var existingAngle = 0;
-
-class CurveAnimate extends Component {
-    lineSegments = updateLine( existingAngle, lineSegments, lineCount );
-    lineCount = lineCount + 1;
-    lineCoord = lineCoord + lineSegments[ ( lineCount / 2 ) ] + "," + lineSegments[ ( ( lineCount / 2 ) + 1 ) ] + " ";
     render() {
-        console.log( "Curve Animate" );
-        console.log( "Inherited counter: " + this.props.counter );
-        console.log( "Existing Angle: " + existingAngle );
-        console.log( "Segments: " + lineSegments );
-        console.log( "Count: " + lineCount );
+        console.log( "DragonCurve.js" );
         return (
-            <svg  >
-                <polyline points={ lineCoord }
-                    fill="transparent" stroke="red" transform="translate(250 250) rotate(180) scale(10)" strokeWidth="4" />
+            /*
+            <svg>
+                <polyline points="20,20 40,25 60,40 80,120 120,140 200,180"
+                    fill="transparent" stroke="red" strokeWidth="4" />
             </svg>
+            */
+            < CurveAnimate width="500" height="500" counter="0" />
         );
     }
+}
+
+var lineSegments = [ 0, 0, 0, 10 ];
+var lineCount = ( lineSegments.length / 2 - 1 );
+var lineCoord = "0,0 0,10";
+var existingAngle = 0;
+var tick = 0;
+function CurveAnimate() {
+    lineSegments = updateLine( existingAngle, lineSegments, lineCount );
+    lineCount = lineCount + 1;
+    console.log( "Count: " + lineCount );
+    console.log( "line coord a: " + lineSegments[ lineCount ] );
+    console.log( "line coord b: " + lineSegments[ ( lineCount + 1 ) ] );
+    lineCoord = lineCoord + " " + lineSegments[ lineCount ] + "," + lineSegments[ lineCount + 1 ];
+    console.log( "lineCoord: " + lineCoord );
+    tick = tick + 1;
+    return (
+        <div>
+            <h1>{ tick }</h1>
+            < svg >
+                <polyline points={ lineCoord }
+                    fill="transparent" stroke="red" transform="translate(250 250) rotate(180) scale(10)" strokeWidth="4" />
+            </svg >
+        </div>
+    );
 }
 
 /*
@@ -40,28 +46,33 @@ lineSegments: array of existing lines
 lineCount: number of existing lines
 return: updated array of lineSegments
 */
+var turnArray = [];
 function updateLine( existingAngle, lineSegments, lineCount ) {
     console.log( "Update Line" );
+    console.log( "Line Count: " + lineCount );
+    console.log( "Turn Array: " + turnArray );
     /// If lineCount is even or 1 ///
     if ( ( lineCount % 2 ) === 0 || lineCount === 1 ) {
         existingAngle = angleCalc( existingAngle, 90 );
+        turnArray.push( 0 );
     } else {
         existingAngle = angleCalc( existingAngle, -90 );
+        turnArray.push( 1 );
     }
     /// Append new coordinate to lineSegments based on Angle rotation ///
     console.log( "Existing angle: " + existingAngle );
     switch ( existingAngle ) {
         case 0:
-            lineSegments.push( 1, 0 );
+            lineSegments.push( 10, 0 );
             break;
         case 90:
-            lineSegments.push( 0, 1 );
+            lineSegments.push( 0, 10 );
             break;
         case 180:
-            lineSegments.push( -1, 0 );
+            lineSegments.push( -10, 0 );
             break;
         case 270:
-            lineSegments.push( 0, -1 );
+            lineSegments.push( 0, -10 );
             break;
         default:
             console.log( "Error: nonacceptable angle" );
--- reactjstest/src/index.js
+++ reactjstest/src/index.js
@@ -4,32 +4,13 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DragonCurve from './DragonCurve';
 
-/*
-function tick( frame ) {
-    frame = frame + 1;
-    console.log( "tick frame: " + frame );
-    ReactDOM.render(
-        <DragonCurve frame={ frame } />,
-        document.getElementById( 'root' )
-    );
+function tick() {
+    console.log( "<" );
+    ReactDOM.render( <DragonCurve />, document.getElementById( 'root' ) );
+    console.log( "/>" );
 }
-*/
-function tick( count ) {
-    count = count + 1;
-    ReactDOM.render(
-        <DragonCurve frame={ count } />,
-        document.getElementById( 'root' )
-    );
-}
-setInterval( tick(), 1000 );
-//var frame = setInterval( tick( frame ), 100 );
 
-console.log( "end" );
-/*
-console.log( "index.js" );
-ReactDOM.render( <DragonCurve />, document.getElementById( 'root' ) );
-console.log( "done" );
-*/
+setInterval( tick, 1000 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
